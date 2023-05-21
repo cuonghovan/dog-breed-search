ef9d792a119a22bbd56d4558cb626b92c101dac4
Cuong Ho <cuonghv21@gmail.com>
2023-05-21 22:06:16 +0800

    Init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,101 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, within } from '@testing-library/react'
+import axios from 'axios'
+import App from './App'
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('axios')
+
+describe('App', () => {
+  test('render app correctly', async () => {
+    render(<App />)
+    
+    expect(screen.getByText('Search Dogs')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Enter breed name')).toBeInTheDocument()
+    expect(screen.getByRole('combobox')).toBeInTheDocument()
+    expect(screen.getByRole('table')).toBeInTheDocument()
+    expect(screen.getByRole('cell')).toHaveTextContent('No dog to be shown')
+  })
+
+  test('render app and fetch dog breeds with images then sort results', async () => {
+    jest.useFakeTimers();
+    
+    const mockBreeds: any[] = [
+      {
+        id: 1,
+        name: 'Bulldog A',
+        height: { imperial: '12 - 16' },
+        life_span: '6 - 10 years',
+        reference_image_id: 'abc1',
+      },
+      {
+        id: 2,
+        name: 'Bulldog B',
+        height: { imperial: '10 - 16' },
+        life_span: '5 - 10 years',
+        reference_image_id: 'abc2',
+      },
+      {
+        id: 3,
+        name: 'Bulldog C',
+        height: { imperial: '13 - 16' },
+        life_span: '4 - 10 years',
+        reference_image_id: 'abc3',
+      },
+    ];
+    const mockImageA: any = {
+      id: 'abc1',
+      url: 'https://example.com/bulldogA.png'
+    };
+    const mockImageB: any = {
+      id: 'abc2',
+      url: 'https://example.com/bulldogB.png'
+    };
+    const mockImageC: any = {
+      id: 'abc3',
+      url: 'https://example.com/bulldogC.png'
+    };
+
+    (axios.get as jest.Mock).mockImplementation((url) => {
+      switch (url) {
+        case '/breeds/search?q=bulldog':
+          return Promise.resolve({ data: mockBreeds });
+        case '/images/abc1':
+          return Promise.resolve({ data: mockImageA })
+        case '/images/abc2':
+          return Promise.resolve({ data: mockImageB })
+        case '/images/abc3':
+          return Promise.resolve({ data: mockImageC })
+        default:
+          return Promise.reject(new Error('not found'))
+      }
+    })
+
+    render(<App />)
+
+    // Search for dog breeds
+    userEvent.type(screen.getByPlaceholderText('Enter breed name'), 'bulldog')
+    jest.advanceTimersByTime(1000);
+
+    await screen.findByText('Bulldog A')
+    expect(screen.getByAltText('Bulldog A')).toHaveAttribute('src', 'https://example.com/bulldogA.png')
+    expect(screen.getByAltText('Bulldog B')).toHaveAttribute('src', 'https://example.com/bulldogB.png')
+    expect(screen.getByAltText('Bulldog C')).toHaveAttribute('src', 'https://example.com/bulldogC.png')
+
+
+    const sortCases = [
+      { sortBy: 'name',  firstDogName: 'Bulldog A'},
+      { sortBy: 'height',  firstDogName: 'Bulldog B'},
+      { sortBy: 'lifespan',  firstDogName: 'Bulldog C'}
+    ]
+    // Sort dog breeds
+    sortCases.forEach(sortData => {
+      userEvent.selectOptions(screen.getByRole('combobox'), sortData.sortBy)
+      const firstRow = screen.getAllByRole('row')[1]
+      const firstDescriptionCell = within(firstRow).getAllByRole('cell')[1]
+      const textContent = within(firstDescriptionCell).getByText(sortData.firstDogName)
+      expect(!!textContent).toBe(true)
+    })
+
+    jest.useRealTimers();
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,162 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import defaultImage from './images/dog.png'
+import './App.css'
+
+enum SORT_OPTION {
+  NAME = 'name',
+  HEIGHT = 'height',
+  LIFESPAN = 'lifespan' 
+}
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [breeds, setBreeds] = useState([])
+  const [selectedSortOption, setSelectedSortOption] = useState<SORT_OPTION>(SORT_OPTION.NAME)
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
+    setSearchTerm(e.target.value)
+  }
+
+  useEffect(() => {
+    const searchDelay = setTimeout(() => {
+      const baseRequestOptions = {
+        baseURL: 'https://api.thedogapi.com/v1',
+        headers: {
+          'x-api-key': process.env.REACT_APP_DOG_API_KEY
+        }
+      }
+
+      axios.get('/breeds/search?q=' + searchTerm, {
+        ...baseRequestOptions,
+      }).then(async function(response) {
+        const breeds = response.data
+        const imageMap = new Map()
+        const imgRequests = breeds.map((breed:any) => {
+          if (breed.reference_image_id) {
+            return axios.get('/images/' + breed.reference_image_id, {
+              ...baseRequestOptions,
+            }).then(function(response) {
+              const image = response.data
+              if(!imageMap.has(image.id)) {
+                imageMap.set(image.id, image.url)
+              }
+            }).catch(function(error) {
+              console.log(error)
+            })
+          }
+          return null
+        })
+
+        // Wait until all images downloaded
+        await Promise.all(imgRequests)
+        breeds.forEach((breed: any, index: number) => {
+          breeds[index] = {...breed, imageUrl: breed.reference_image_id ? imageMap.get(breed.reference_image_id) : defaultImage}
+        })
+        setBreeds(breeds)
+        setSelectedSortOption(SORT_OPTION.NAME)
+      }).catch(function(error) {
+        console.log(error)
+      })
+    }, 1000)
+
+    return () => clearTimeout(searchDelay)
+  }, [searchTerm])
+
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOption = e.target.value as SORT_OPTION
+    setSelectedSortOption(selectedOption)
+
+    switch(selectedOption) {
+      case SORT_OPTION.NAME:
+        sortByName()
+        break
+      case SORT_OPTION.HEIGHT:
+        sortByHeight()
+        break
+      case SORT_OPTION.LIFESPAN:
+        sortByLifespan()
+        break
+      default:
+        sortByName()
+        break
+    }
+  }
+
+  const sortByName = () => {
+    const sortedBreeds = [...breeds]
+    sortedBreeds.sort((breedA: any, breedB: any) => {
+      if (breedA.name < breedB.name) return -1;
+      if (breedA.name > breedB.name) return 1;
+      return 0;
+    })
+    setBreeds(sortedBreeds)
+  }
+
+  const sortByHeight = () => {
+    const sortedBreeds = [...breeds]
+    sortedBreeds.sort((breedA: any, breedB: any) => {
+      const imperialA = parseFloat(breedA.height.imperial.split(' - ')[0])
+      const imperialB = parseFloat(breedB.height.imperial.split(' - ')[0])
+      return imperialA - imperialB
+    })
+    setBreeds(sortedBreeds)
+  }
+
+  const sortByLifespan = () => {
+    const sortedBreeds = [...breeds]
+    sortedBreeds.sort((breedA: any, breedB: any) => {
+      const lifespanA = parseFloat(breedA.life_span.match(/\d+/g)[0])
+      const lifespanB = parseFloat(breedB.life_span.match(/\d+/g)[0])
+      return lifespanA - lifespanB
+    })
+    setBreeds(sortedBreeds)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto px-4 min-h-screen bg-cyan-50'>
+      <header className='flex justify-center pt-16 text-3xl capitalize'>Search Dogs</header>
+      <section className='flex justify-center mt-16'>
+          <input className='w-80 p-4 rounded-md' type='text' placeholder='Enter breed name' value={searchTerm} onChange={handleSearchChange} />
+          <select className='w-30 p-4 ml-4 bg-white' value={selectedSortOption} onChange={handleSortChange}>
+            <option value={SORT_OPTION.NAME} defaultChecked>Name</option>
+            <option value={SORT_OPTION.HEIGHT}>Height</option>
+            <option value={SORT_OPTION.LIFESPAN}>Lifespan</option>
+          </select>
+      </section>
+      <section className='mt-16'>
+          <table className='table-auto min-w-full boder-b-2 border-gray-200'>
+            <thead>
+              <tr>
+                <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 w-1/3 text-left text-xs font-semibold text-gray-700 uppercase'>Image</th>
+                <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase'>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {breeds.length > 0 ?
+                <>
+                  {breeds.map((breed: any) => (
+                    <tr key={breed.id}>
+                      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'><img src={breed.imageUrl} alt={breed.name} width='300' height='auto' /></td>
+                      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                        <ul>
+                          <li><b>Name:</b> {breed.name}</li>
+                          <li><b>Height:</b> {breed.height.imperial} inches</li>
+                          <li><b>Lifespan:</b> {breed.life_span}</li>
+                        </ul>
+                      </td>
+                    </tr>
+                  ))}
+                </>
+                : <tr>
+                  <td colSpan={2} className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>No dog to be shown</td>
+                </tr>
+              }
+            </tbody>
+          </table>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
